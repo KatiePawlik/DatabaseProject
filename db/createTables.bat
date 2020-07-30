e45871a6413bd2d1230c8879f87950d8a2d93fbc
Curtis Sapp <curtmsapp@gmail.com>
2020-07-30 12:46:17 -0700

    Added Pawlik Labs database creation script
    Changed name of database from "testDB.db" to "PawlikLabs.db
    Updated db READ_ME to match
    Changed db READ_ME to text file
    Created medical database models
    Updated create database script to match medical models

--- db/createTables.bat
+++ db/createTables.bat
@@ -1,3 +1,4 @@
-sqlite3 ../testDB.db ""
-sqlite3 ../testDB.db ".read createTables.sql"
+del /Q "..\PawlikLabs.db"
+sqlite3 ../PawlikLabs.db ""
+sqlite3 ../PawlikLabs.db ".read createTables.sql"
 pause
--- db/createTables.sql
+++ db/createTables.sql
@@ -1,1 +1,40 @@
-CREATE TABLE IF NOT EXISTS "medical" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "msg" VARCHAR(45) );
+CREATE TABLE IF NOT EXISTS "users" ( "id" INTEGER PRIMARY KEY AUTOINCREMENT, "name" VARCHAR(45), "password_hash" CHAR(64));
+
+CREATE TABLE IF NOT EXISTS "staff" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "name" VARCHAR(45),
+  "password_hash" VARCHAR(64));
+  
+CREATE TABLE IF NOT EXISTS "med_vist" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "notes" VARCHAR(160),
+  "patient_id" INTEGER,
+  "doctor_id" INTEGER,
+ 
+  CONSTRAINT "patient_fk"
+    FOREIGN KEY ("patient_id")
+    REFERENCES "users" ("id")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+  CONSTRAINT "doctor_fk"
+    FOREIGN KEY ("doctor_id")
+    REFERENCES "staff" ("id")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE);
+
+CREATE INDEX "doctor_fk_idx" ON "med_vist"("doctor_id" ASC);
+CREATE INDEX "patient_fk_idx"  ON "med_vist"("patient_id" ASC);
+
+CREATE TABLE IF NOT EXISTS "med_report" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "report_data" VARCHAR(400),
+  "visit_id" INTEGER,
+
+  CONSTRAINT "visit_fk"
+    FOREIGN KEY ("visit_id")
+    REFERENCES "med_vist" ("id")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE);
+
+ CREATE INDEX "visit_fk_idx" ON "med_report"("visit_id" ASC);
+
