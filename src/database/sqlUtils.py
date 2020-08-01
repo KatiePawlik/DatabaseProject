2cfa508ae3054c281850e91290ed3fce6ce1774a
Curtis Sapp <curtmsapp@gmail.com>
2020-08-01 13:41:18 -0700

    Database path is now machine independent

--- src/database/sqlUtils.py
+++ src/database/sqlUtils.py
@@ -1,17 +1,20 @@
 import sqlite3
 from sqlite3 import Error
+from utils.fileUtils import getDatabasePath
+
 
 class sqlUtils():
     def __init__(self):
         print("hi")
+
     def addUserToDB(self, user):
         name = user.name
         identifier = user.id
         password = user.password_hash
-
-        self.database = r"C:/git/Git-Practice/DatabaseProject/PawlikLabs.db"
+        # C:/git/Git-Practice/DatabaseProject/PawlikLabs.db
+        self.database = getDatabasePath()
         self.conn = None
-        self.user = [(identifier, name, password),]   
+        self.user = [(identifier, name, password), ]
         try:
             self.conn = sqlite3.connect(self.database)
             self.sql = '''INSERT INTO users(id, name, password_hash) VALUES(?,?,?)'''
@@ -19,9 +22,9 @@ class sqlUtils():
             self.cur.executemany(self.sql, self.user)
             self.conn.commit()
 
-        except Error as e: 
+        except Error as e:
             # enable for debugging:
             print(e)
             return False
-        
+
         return self.cur.lastrowid
